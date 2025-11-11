321e642f1f0931625cc4be711179ed14839aacd7
KarlDragon <giocodeproject@gmail.com>
2025-11-12 02:40:04 +0700

    Create: Simple homepage UI :V I hate this part

--- Clients/src/components/Homepage/Homepage.tsx
+++ Clients/src/components/Homepage/Homepage.tsx
@@ -1,26 +1,43 @@
 import { Logout } from "../Logout"
 import { useAuthContext } from "../../hooks/AuthContext";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import './Homepage.css'
 export { Homepage }
 
 function Homepage() {
   const navigate = useNavigate();
+  const [hierarchyState, setHierarchyState] = useState<"daily" | "monthly" | "yearly">("daily");
   const { setUserEmail } = useAuthContext();
   return (
     <div className="homepageContainer">
       <header className="homepageHeader">
         <h1>ManageMe</h1>
-        <button onClick={() => { Logout({ setUserEmail }, navigate) }}>Log out</button>
+        <button onClick={() => { Logout({ setUserEmail }, navigate) }}>Đăng xuất</button>
       </header>
       <main className="homepageMain">
-        <div className="hierarchy">
+        <div className="feature">
+          <div className="hierarchy">
+            <h2>Chi tiêu:</h2>
+            <div className="expensetimeWrapper">
+              <button onClick={() => setHierarchyState("daily")}>Hằng Ngày</button>
+              <button onClick={() => setHierarchyState("monthly")}>Hằng Tháng</button>
+              <button onClick={() => setHierarchyState("yearly")}>Hằng Năm</button>
+            </div>
 
+          </div>
+          <div className="contentArea">
+            {hierarchyState === "daily" ? (
+              <div>Giao diện chi tiêu hằng ngày</div>
+            ) : hierarchyState === "monthly" ? (
+              <div>Giao diện chi tiêu hằng tháng</div>
+            ) : (
+              <div>Giao diện chi tiêu hằng năm</div>
+            )}
+          </div>
         </div>
-        <div className="contentArea">
 
-        </div>
-      </main> 
+      </main>
     </div>
   )
 }
