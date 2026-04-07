8371c454e5ad9a071cdab0bf8eac66be7ac3e043
KarlDragon <giocodeproject@gmail.com>
2026-04-07 21:42:48 +0700

    None

--- Server/Services/ISpendingService.cs
+++ Server/Services/ISpendingService.cs
@@ -15,11 +15,4 @@ public interface ISpendingService
     /// <param name="hierarchy">The time period hierarchy (daily, monthly, yearly).</param>
     /// <returns>A task containing the spending data with total, byCategory, and byItems.</returns>
     Task<object> GetSpending(string userId, string hierarchy);
-
-    /// <summary>
-    /// Modifies an existing user content item with new data.
-    /// </summary>
-    /// <param name="dto">The data transfer object containing the updated content information.</param>
-    /// <returns>A task representing the asynchronous operation, containing the result of the modification.</returns
-    Task<object> ModifyUserContent(UserContentDto dto);
 }
